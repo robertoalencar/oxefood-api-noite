dc4420b0324a6df27bf3e5b764117c58a27f6934
Roberto Alencar <rroberto.alencar@gmail.com>
2024-11-11 19:31:10 -0300

    enviando alterações do projeto

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -1,5 +1,31 @@
 package br.com.ifpe.oxefood.api.cliente;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+
+@RestController
+@RequestMapping("/api/cliente")
+@CrossOrigin
 public class ClienteController {
     
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+
+        Cliente clienteNovo = request.build();
+        Cliente cliente = clienteService.save(clienteNovo);
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+    }
+
 }
