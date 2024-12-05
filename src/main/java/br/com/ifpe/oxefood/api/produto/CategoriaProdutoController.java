4edb2550620b34137917d01eb7c464b5abc61758
Roberto Alencar <rroberto.alencar@gmail.com>
2024-12-05 20:20:15 -0300

    Enviando arquivos da aula

--- src/main/java/br/com/ifpe/oxefood/api/produto/CategoriaProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/CategoriaProdutoController.java
@@ -15,59 +15,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifpe.oxefood.modelo.produto.CategoriaProduto;
 import br.com.ifpe.oxefood.modelo.produto.CategoriaProdutoService;
-import br.com.ifpe.oxefood.modelo.produto.Produto;
-import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/produto")
+@RequestMapping("/api/categoriaproduto")
 @CrossOrigin
-public class ProdutoController {
-
-    @Autowired
-    private ProdutoService produtoService;
+public class CategoriaProdutoController {
 
     @Autowired
     private CategoriaProdutoService categoriaProdutoService;
 
     @PostMapping
-    public ResponseEntity<Produto> save(@RequestBody @Valid ProdutoRequest request) {
+    public ResponseEntity<CategoriaProduto> save(@RequestBody @Valid CategoriaProdutoRequest request) {
 
-        Produto produtoNovo = request.build();
-        produtoNovo.setCategoria(categoriaProdutoService.obterPorID(request.getIdCategoria()));
-        Produto produto = produtoService.save(produtoNovo);
-
-        return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
+        CategoriaProduto categoriaProdutoNovo = request.build();
+        CategoriaProduto categoriaProduto = categoriaProdutoService.save(categoriaProdutoNovo);
+        return new ResponseEntity<CategoriaProduto>(categoriaProduto, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<Produto> listarTodos() {
-
-        return produtoService.listarTodos();
+    public List<CategoriaProduto> listarTodos() {
+  
+       return categoriaProdutoService.listarTodos();
     }
 
     @GetMapping("/{id}")
-    public Produto obterPorID(@PathVariable Long id) {
+    public CategoriaProduto obterPorID(@PathVariable Long id) {
 
-        return produtoService.obterPorID(id);
+       return categoriaProdutoService.obterPorID(id);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Produto> update(@PathVariable("id") Long id, @RequestBody ProdutoRequest request) {
+    public ResponseEntity<CategoriaProduto> update(@PathVariable("id") Long id, @RequestBody CategoriaProdutoRequest request) {
 
-        Produto produto = request.build();
-        produto.setCategoria(categoriaProdutoService.obterPorID(request.getIdCategoria()));
-        produtoService.update(id, produto);
-
+        categoriaProdutoService.update(id, request.build());
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
 
-        produtoService.delete(id);
+        categoriaProdutoService.delete(id);
         return ResponseEntity.ok().build();
     }
-
+    
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/produto/CategoriaProduto.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/CategoriaProduto.java
@@ -5,7 +5,6 @@ import org.hibernate.annotations.SQLRestriction;
 import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,34 +13,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "Produto")
+@Table(name = "CategoriaProduto")
 @SQLRestriction("habilitado = true")
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Produto extends EntidadeAuditavel  {
-
-    @ManyToOne
-    private CategoriaProduto categoria;
-    
-    @Column
-    private String codigo;
-    
-    @Column
-    private String titulo;
+public class CategoriaProduto extends EntidadeAuditavel {
     
     @Column
     private String descricao;
-    
-    @Column
-    private Double valorUnitario;
-    
-    @Column
-    private Integer tempoEntregaMinimo;
-    
-    @Column
-    private Integer tempoEntregaMaximo;
-    
+
 }
