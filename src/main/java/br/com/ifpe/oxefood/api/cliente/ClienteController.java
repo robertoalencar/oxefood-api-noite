99f35a41ac775c04df56f450bab6a7cca4660881
Roberto Alencar <rroberto.alencar@gmail.com>
2024-12-19 16:23:30 -0300

    enviando arquivos da aula

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+import br.com.ifpe.oxefood.modelo.cliente.EnderecoCliente;
 import jakarta.validation.Valid;
 
 @RestController
@@ -60,6 +61,27 @@ public class ClienteController {
 
         clienteService.delete(id);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/endereco/{clienteId}")
+    public ResponseEntity<EnderecoCliente> adicionarEnderecoCliente(@PathVariable("clienteId") Long clienteId, @RequestBody @Valid EnderecoClienteRequest request) {
+
+        EnderecoCliente endereco = clienteService.adicionarEnderecoCliente(clienteId, request.build());
+        return new ResponseEntity<EnderecoCliente>(endereco, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/endereco/{enderecoId}")
+    public ResponseEntity<EnderecoCliente> atualizarEnderecoCliente(@PathVariable("enderecoId") Long enderecoId, @RequestBody EnderecoClienteRequest request) {
+
+        EnderecoCliente endereco = clienteService.atualizarEnderecoCliente(enderecoId, request.build());
+        return new ResponseEntity<EnderecoCliente>(endereco, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/endereco/{enderecoId}")
+    public ResponseEntity<Void> removerEnderecoCliente(@PathVariable("enderecoId") Long enderecoId) {
+
+        clienteService.removerEnderecoCliente(enderecoId);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
@@ -1,13 +1,17 @@
 package br.com.ifpe.oxefood.api.cliente;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import br.com.ifpe.oxefood.modelo.acesso.Perfil;
+import br.com.ifpe.oxefood.modelo.acesso.Usuario;
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -21,6 +25,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClienteRequest {
+
+    @NotBlank(message = "O e-mail é de preenchimento obrigatório")
+    @Email
+    private String email;
+
+    @NotBlank(message = "A senha é de preenchimento obrigatório")
+    private String password;
 
     @NotNull(message = "O Nome é de preenchimento obrigatório")
     @NotEmpty(message = "O Nome é de preenchimento obrigatório")
@@ -39,16 +50,26 @@ public class ClienteRequest {
 
     private String foneFixo;
 
-        public Cliente build() {
+    public Usuario buildUsuario() {
 
-            return Cliente.builder()
-                .nome(nome)
-                .dataNascimento(dataNascimento)
-                .cpf(cpf)
-                .foneCelular(foneCelular)
-                .foneFixo(foneFixo)
-                .build();
-        }
+        return Usuario.builder()
+            .username(email)
+            .password(password)
+            .roles(Arrays.asList(new Perfil(Perfil.ROLE_CLIENTE)))
+            .build();
+    }
+
+    public Cliente build() {
+
+        return Cliente.builder()
+            .usuario(buildUsuario())
+            .nome(nome)
+            .dataNascimento(dataNascimento)
+            .cpf(cpf)
+            .foneCelular(foneCelular)
+            .foneFixo(foneFixo)
+            .build();
+    }
 
 }
 
